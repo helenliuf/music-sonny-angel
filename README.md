a737530258c203693104969cd90d4adb40a9e94b
helenliuf <64387336+helenliuf@users.noreply.github.com>
2023-12-04 03:15:01 -0500

    Update README.md with presentation
    Co-Authored-By: Helen Liu <helenfayliu@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Music Sonny Angel - CICS256 Final Assignment
 Group members:\
 Helen Liu, Larissa Zhu, Katie Xu, Camden Alexander
+<<<<<<< HEAD
 \
 <<<<<<< HEAD
 \
@@ -28,6 +29,9 @@ There's never a dull moment here! Designed to bring happiness to the music enthu
 4. Upload program to Makerboard
 5. Enjoy the company of your new friend!
 =======
+=======
+\\
+>>>>>>> f399ea4 (Update README.md with presentation)
 Presentation link:\
 https://www.canva.com/design/DAF1_amApRo/uNLPFWbWSq5N-Fl6BiW-HA/view?utm_content=DAF1_amApRo&utm_campaign=designshare&utm_medium=link&utm_source=editor
 >>>>>>> cd64344 (Update README.md with presentation link)
--- final/final.ino
+++ final/final.ino
@@ -31,11 +31,11 @@ uint8_t hue = 0;
 CRGB leds[NUM_LEDS];
 CRGB ring1[16];
 CRGB ring2[16];
-//
-//#define ringshow_noglitch() {delay(1);ring.show();delay(1);ring.show();}
-//#define ringshow_noglitch2() {delay(1);ring2.show();delay(1);ring2.show();}
-//Adafruit_NeoPixel ring = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel ring2 = Adafruit_NeoPixel(16, PIN2, NEO_GRB + NEO_KHZ800);
+
+const int sweepDuration = 5; // Adjust the speed of the sweep in milliseconds
+unsigned long previousMillis = 0;
+bool isForwardSweep = true;
+
 Servo servo;
 
 int samples[N]; // storage for a sample
@@ -64,8 +64,7 @@ void setup() {
   FastLED.addLeds<WS2812B, 25, GRB>(ring2, NUM_LEDS);
   FastLED.clear();
 
-   servo.attach(18); 
-
+  servo.attach(18); 
 }
 
 float getAvg() {
@@ -83,7 +82,7 @@ void loop() {
   queue[queue_index] = amp;
   queue_index = (queue_index + 1) % QSIZE;
   int avg = getAvg();
-  Serial.println(avg);
+  //Serial.println(avg);
 
   sweepServo();
 
@@ -98,36 +97,23 @@ void loop() {
   }
     
   //button debounce
-  if (millis() > (timeout+100)) {
+  if (millis() > (timeout+150)) {
     int button1 = digitalRead(BUTTON);
     Serial.println(button1);
     if (button1 == 0) {
-      strip_mode *= 1;
+      if (strip_mode == 4){
+        strip_mode = 1;
+      } else{
+        strip_mode += 1;
+      }
+      
     }
    timeout = millis();
   }
 
-  // Map the filtered sensor value to the number of LEDs
-   int mappedValue = map(avg, 40, 1000, 0, N_PIXELS / 2);
+  FastLED.setBrightness(32);
 
-  if (strip_mode == 1) {
-    for (int i = 0; i < N_PIXELS; i++) {
-      if (i < mappedValue && (i < (N_PIXELS / 2))) {
-        leds[i] = 0x00FF00;  // Change the color as needed (here: red)
-      } else if (i > (N_PIXELS - mappedValue - 1)) {
-        leds[i] = 0xFF0000;  // Change the color as needed (here: red)
-      }
-      else {
-        leds[i] = 0xFFFFFF;  // Change the color as needed (here: red)
-      }
-    }
-  } 
-  else {
-    rainbowCycle(1);
-  }
-  
-  mappedValue = map(avg, 100, 1000, 0, 16);
-
+  int mappedValue = map(avg, 100, 1000, 0, 16);
   for(int i=0;i<(16);i++) {
     if (i < mappedValue) {
         ring1[i] = 0xFFFF00;
@@ -143,9 +129,40 @@ void loop() {
       ring2[i] = 0xFFFFFF;
     }
   }
-  
-  FastLED.setBrightness(32);
-  FastLED.show();
+
+  // Map the filtered sensor value to the number of LEDs
+  mappedValue = map(avg, 40, 1000, 0, N_PIXELS / 2);
+
+  if (strip_mode == 1) {
+    for (int i = 0; i < N_PIXELS; i++) {
+      if (i < mappedValue && (i < (N_PIXELS / 2))) {
+        leds[i] = 0x00FF00;  // Change the color as needed (here: red)
+      } else if (i > (N_PIXELS - mappedValue - 1)) {
+        leds[i] = 0xFF0000;  // Change the color as needed (here: red)
+      }
+      else {
+        leds[i] = 0xFFFFFF;  // Change the color as needed (here: red)
+      }
+    }
+    FastLED.show();
+  } 
+  else if (strip_mode == 2) {
+    mappedValue = map(avg, 40, 1000, 0, 255);
+    Fire(20, mappedValue, 10);
+
+  } else if (strip_mode == 3){
+    rainbowCycle(1);
+    FastLED.show();
+  } else{
+    int delay_time = 0;
+    mappedValue = map(avg, 40, 1000, 0, 39);
+    if ((40-mappedValue)<0){
+      delay_time = 0;
+    } else{
+      delay_time = 40-mappedValue;
+    }
+    shootingStarAnimation(255, 255, 255, 100, delay_time, 10000, 1);
+  }
 
 }
 
@@ -210,13 +227,113 @@ void rainbowCycle(int wait) {
 }
 
 void sweepServo() {
-  for (int angle = 0; angle <= 180; angle++) {
-    servo.write(angle);  // Set the servo position
-    delay(10);             // Adjust the speed of the sweep
-  }
+  unsigned long currentMillis = millis();
 
-  for (int angle = 180; angle >= 0; angle--) {
-    servo.write(angle);  // Set the servo position
-    delay(10);             // Adjust the speed of the sweep
+  if (currentMillis - previousMillis >= sweepDuration) {
+    previousMillis = currentMillis;
+
+    static int angle = 0;
+
+    if (isForwardSweep) {
+      servo.write(angle);  // Set the servo position
+      angle++;
+
+      if (angle > 180) {
+        angle = 180;
+        isForwardSweep = false; // Change direction to backward sweep
+      }
+    } else {
+      servo.write(angle);  // Set the servo position
+      angle--;
+
+      if (angle < 0) {
+        angle = 0;
+        isForwardSweep = true; // Change direction to forward sweep
+      }
+    }
+  }
+}
+
+
+//The following code is from Electriangle on GitHub
+unsigned long previousMillis2 = 0;           // Stores last time LEDs were updated
+int count = 0;                              // Stores count for incrementing up to the NUM_LEDs
+
+void shootingStarAnimation(int red, int green, int blue, int tail_length, int delay_duration, int interval, int direction){
+  unsigned long currentMillis = millis();   // Get the time
+  if (currentMillis - previousMillis2 >= interval) {
+    previousMillis2 = currentMillis;         // Save the last time the LEDs were updated
+    count = 0;                              // Reset the count to 0 after each interval
+  }
+  if (direction == -1) {        // Reverse direction option for LEDs
+    if (count < NUM_LEDS) {
+      leds[NUM_LEDS - (count % (NUM_LEDS+1))].setRGB(red, green, blue);    // Set LEDs with the color value
+      count++;
+    }
+  }
+  else {
+    if (count < NUM_LEDS) {     // Forward direction option for LEDs
+      leds[count % (NUM_LEDS+1)].setRGB(red, green, blue);    // Set LEDs with the color value
+      count++;
+    }
+  }
+  fadeToBlackBy(leds, NUM_LEDS, tail_length);                 // Fade the tail LEDs to black
+  FastLED.show();
+  delay(delay_duration);                                      // Delay to set the speed of the animation
+}
+
+void Fire(int FlameHeight, int Sparks, int DelayDuration) {
+  static byte heat[NUM_LEDS];
+  int cooldown;
+  
+  // Cool down each cell a little
+  for(int i = 0; i < NUM_LEDS; i++) {
+    cooldown = random(0, ((FlameHeight * 10) / NUM_LEDS) + 2);
+   
+    if(cooldown > heat[i]) {
+      heat[i] = 0;
+    }
+    else {
+      heat[i] = heat[i] - cooldown;
+    }
+  }
+  
+  // Heat from each cell drifts up and diffuses slightly
+  for(int k = (NUM_LEDS - 1); k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+  }
+  
+  // Randomly ignite new Sparks near bottom of the flame
+  if(random(255) < Sparks) {
+    int y = random(7);
+    heat[y] = heat[y] + random(160, 255);
+  }
+  
+  // Convert heat to LED colors
+  for(int j = 0; j < NUM_LEDS; j++) {
+    setPixelHeatColor(j, heat[j]);
+  }
+  
+  FastLED.show();
+  delay(DelayDuration);
+}
+
+void setPixelHeatColor(int Pixel, byte temperature) {
+  // Rescale heat from 0-255 to 0-191
+  byte t192 = round((temperature / 255.0) * 191);
+  
+  // Calculate ramp up from
+  byte heatramp = t192 & 0x3F; // 0...63
+  heatramp <<= 2; // scale up to 0...252
+  
+  // Figure out which third of the spectrum we're in:
+  if(t192 > 0x80) {                    // hottest
+    leds[Pixel].setRGB(255, 255, heatramp);
+  }
+  else if(t192 > 0x40) {               // middle
+    leds[Pixel].setRGB(255, heatramp, 0);
+  }
+  else {                               // coolest
+    leds[Pixel].setRGB(heatramp, 0, 0);
   }
 }
